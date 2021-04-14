21185f21fcedd6b297eddcc154cefcd489e2d7a1
Mary Toth <maryarylynn@gmail.com>
2021-04-14 12:41:44 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AllCardsOnDeckCS.Models;
+using System.Linq;
+
+namespace AllCardsOnDeckCS
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+
+    }
+  }
+}
